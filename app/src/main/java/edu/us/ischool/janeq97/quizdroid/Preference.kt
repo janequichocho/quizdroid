7d26fba53f455ad2532dc52e171fe0661253bc13
Yvonne61m <yvonne61m@gmail.com>
2019-05-09 22:14:11 -0700

    add menu, bug need to be fix

--- app/src/main/java/edu/us/ischool/janeq97/quizdroid/Preference.kt
+++ app/src/main/java/edu/us/ischool/janeq97/quizdroid/Preference.kt
@@ -1,5 +1,6 @@
 package edu.us.ischool.janeq97.quizdroid
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -9,9 +10,10 @@ class Preference: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.preference)
 
-        val btnPreference = findViewById<Button>(R.id.btnPreference)
-        btnPreference.setOnClickListener() {
-
-        }
+//        val btnPreference = findViewById<Button>(R.id.btnPreference)
+//        btnPreference.setOnClickListener() {
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
