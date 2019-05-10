bb80e86534019711583704a17812fa0acd938659
Yvonne61m <yvonne61m@gmail.com>
2019-05-09 20:57:01 -0700

    add preference layout and activity

---
+++ app/src/main/java/edu/us/ischool/janeq97/quizdroid/Preference.kt
@@ -0,0 +1,17 @@
+package edu.us.ischool.janeq97.quizdroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+class Preference: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.preference)
+
+        val btnPreference = findViewById<Button>(R.id.btnPreference)
+        btnPreference.setOnClickListener() {
+
+        }
+    }
+}
